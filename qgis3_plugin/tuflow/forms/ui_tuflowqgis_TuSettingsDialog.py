9a5bf700f94666b31822bfb7e452236a76c129c8
EllisSymons <ellis.symons88@gmail.com>
2019-05-07 12:22:38 +1000

    Repository management - QGIS 2.18 no longer LTR and TUFLOW plugin for QGIS 2 is no longer being supported and updated

--- qgis3_plugin/tuflow/forms/ui_tuflowqgis_TuSettingsDialog.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'C:\Users\Ellis.Symons\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins\tuflow\forms\ui_tuflowqgis_TuSettingsDialog.ui'
-#
-# Created by: PyQt5 UI code generator 5.9
-#
-# WARNING! All changes made in this file will be lost!
-
--- qgis3_plugin/tuflow/utm/utm/__init__.py
+++
@@ -1,2 +0,0 @@
-from tuflow.utm.utm.conversion import to_latlon, from_latlon, latlon_to_zone_number, latitude_to_zone_letter, check_valid_zone
-from tuflow.utm.utm.error import OutOfRangeError
